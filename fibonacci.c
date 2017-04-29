9b0cb75f89219b93f311b8e303ad52c75ec82b53
fjimenez94 <fjimenez94@hotmail.com>
2017-04-29 17:04:51 -0600

    Inicio problema 3

---
+++ fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    // Declara variables de distintos tipos de datos
+   
+
+	int f1 = 1;
+	int f2 = 1;
+	int term;
+	
+printf("Ingrese número de datos deseado: ");
+    scanf("%d", &term);
+	
+if (term>0)
+{
+
+}
+else
+{
+printf("Dato ingresado invalido.");
+}
+	
+  
+
+
+
+ 
+    return 0;
+}
