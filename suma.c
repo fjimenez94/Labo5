54d45298b17cade6c3c25cfb5f698a705c09c8d2
fjimenez94 <fjimenez94@hotmail.com>
2017-04-29 15:41:11 -0600

    primer commit

---
+++ suma.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    // Declara variables de distintos tipos de datos
+   
+
+	int a = 7;
+	int b = 15;
+	int c;
+	 
+	c = a+b;
+
+	    
+    // Imprime el valor de a y b
+    // Imprime la suma    
+
+
+printf("a = %i\n", a);
+  printf("b = %i\n", b);
+    printf("%i + %i = %i\n", a, b, c);
+ 
+    return 0;
+}
