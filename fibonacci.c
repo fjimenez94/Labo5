f8ced893503b5f31cddf0b8d1a2d98c09c114f50
fjimenez94 <fjimenez94@hotmail.com>
2017-05-01 11:00:10 -0600

    Inicio problema 3

--- fibonacci.c
+++ fibonacci.c
@@ -3,25 +3,47 @@
 
 int main() {
 
-    // Declara variables de distintos tipos de datos
-   
+	    // Declara variables de distintos tipos de datos
+	   
 
-	int f1 = 1;
-	int f2 = 1;
-	int term;
-	
-printf("Ingrese número de datos deseado: ");
-    scanf("%d", &term);
-	
-if (term>0)
-{
+		int f1 = 1;
+		int f2 = 1;
+		int f3 = 2;
+		int term;
 
-}
-else
-{
-printf("Dato ingresado invalido.");
-}
+
+	printf("Ingrese número de datos deseado: ");
+	    scanf("%d", &term);
+
+	if (term>0)
+	{
+	  for(int x=0;x<term;x+=1)
+	    {
+
+		if (x<2)
+		{
+		printf("%d ", f1);
+		}
+		else
+		{
 	
+		f3=f1+f2;
+	
+		f1=f2;
+		f2=f3;
+	
+		printf("%d ", f3);
+	
+		}
+
+	    }
+
+	}
+	else
+	{
+	printf("Dato ingresado invalido.");
+	}
+
   
 
 
